ca1bc1feec748285ea0f691705c928ac4a9fb939
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-29 11:24:07 +0300

    JSX sintax

--- Intro/JSX-demo/src/script.js
+++ Intro/JSX-demo/src/script.js
@@ -1,6 +1,26 @@
-const rootEl = document.getElementById('root');
-const root = React.createRoot(rootEl);
+/* JS */
 
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-const reactEl=React.createElement('h1',{},'Hello!');
-root.render();
+const reactElement = React.createElement('h1', {}, 'Hello from React');
+// const reactElement = React.createElement(
+//   'header',
+//   { className: 'site-header' },
+//   React.createElement('h1', {}, 'Hello from React')
+// );
+
+root.render(reactElement);
+
+/* JSX */
+
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
+
+// const reactElement = <header className="site-header">
+//   <h1>Hello From JSX</h1>
+//   <h2>React slogan</h2>
+
+// </header>
+
+// root.render(reactElement);
