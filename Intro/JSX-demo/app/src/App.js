2afef223c5ef01aaa28336a1616a130fa04c6fe3
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-30 11:23:48 +0300

    create components

---
+++ Intro/JSX-demo/app/src/App.js
@@ -0,0 +1,15 @@
+import Footer from './Footer'
+import './App.css';
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Intro/JSX-demo/app/src/Header.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+
+export default function Header(){
+
+    return  (<header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>Hello</h1>
+  </header>)
+}
